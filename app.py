2c175245d2df6672ac6c729b81f6e5cc426a22a2
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-03-13 19:32:00 +0530

    database setup

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(200),nullable = False)
+    desc = db.Column(db.Integer(500),nullabe = False)
+    date_created = db.Column(db.DateTime,default = datetime.utcnow)
 
 @app.route('/')
 def hello_world():
